7734ead059d3d708bbcc7a217483f770b3bae2bd
Amber Conville <crebma@gmail.com>
2013-05-31 16:29:01 -0400

    readability refactorings

--- Nerd Nite/NNPastEventView.m
+++ Nerd Nite/NNPastEventView.m
@@ -37,26 +37,31 @@
 
 - (void)setEventToView:(NNEvent *)event {
     self.event = event;
+
     [self.eventTitleLabel setText:event.title];
+    [self.descriptionLabel setText:event.about];
+    [self.venueLabel setText:event.venueName];
+    [NNDateLabelFormatter setUpDateLabel:self.eventDateLabel andSuffixLabel:self.eventDateSuffixLabel forDate:event.date];
 
-    NSString *description = event.about;
-    CGRect originalDescriptionFrame = self.descriptionLabel.frame;
-    CGFloat descriptionWidth = originalDescriptionFrame.size.width;
-    CGSize descriptionSize = [description sizeWithFont:self.descriptionLabel.font constrainedToSize:CGSizeMake(descriptionWidth, MAXFLOAT)];
-    [self.descriptionLabel setFrame:(CGRect){originalDescriptionFrame.origin,{descriptionWidth, descriptionSize.height}}];
+    [self resizeLabel:self.descriptionLabel toFitText:event.about];
 
-    float voteButtonY = self.descriptionLabel.frame.origin.y + self.descriptionLabel.frame.size.height + 10;
-    [self.voteButton setFrame:(CGRect) {{self.voteButton.frame.origin.x, voteButtonY}, self.voteButton.frame.size}];
-
-    float picsButtonY = self.voteButton.frame.origin.y + self.voteButton.frame.size.height + 10;
-    [self.picsButton setFrame:(CGRect) {{self.picsButton.frame.origin.x, picsButtonY}, self.picsButton.frame.size}];
+    [self moveViewElement:self.voteButton belowViewElement:self.descriptionLabel];
+    [self moveViewElement:self.picsButton belowViewElement:self.voteButton];
 
     float viewHeight = self.picsButton.frame.origin.y + self.picsButton.frame.size.height + 10;
     [self.scrollView setContentSize:CGSizeMake(self.scrollView.contentSize.width, viewHeight)];
+}
 
-    [self.descriptionLabel setText:description];
-    [self.venueLabel setText:event.venueName];
-    [NNDateLabelFormatter setUpDateLabel:self.eventDateLabel andSuffixLabel:self.eventDateSuffixLabel forDate:event.date];
+- (void)resizeLabel:(UILabel *)label toFitText:(NSString *)text {
+    CGRect originalDescriptionFrame = label.frame;
+    CGFloat descriptionWidth = originalDescriptionFrame.size.width;
+    CGSize descriptionSize = [text sizeWithFont:label.font constrainedToSize:CGSizeMake(descriptionWidth, MAXFLOAT)];
+    [label setFrame:(CGRect) {originalDescriptionFrame.origin, {descriptionWidth, descriptionSize.height}}];
+}
+
+- (void)moveViewElement:(UIView *)bottom belowViewElement:(UIView *)top {
+    float bottomY = top.frame.origin.y + top.frame.size.height + 10;
+    [bottom setFrame:(CGRect) {{bottom.frame.origin.x, bottomY}, bottom.frame.size}];
 }
 
 @end
